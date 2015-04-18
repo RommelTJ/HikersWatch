6c459407c74edf8ca870eac0b47c475c2ff892de
Rommel Rico <rommeltj@gmail.com>
2015-04-17 17:16:14 -0700

    Displaying the info on the watch
    Implementing the Watch Interface

---
+++ HikersWatch WatchKit App/tableRowController.swift
@@ -0,0 +1,16 @@
+//
+//  tableRowController.swift
+//  HikersWatch
+//
+//  Created by Rommel Rico on 4/17/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class tableRowController: NSObject {
+   
+    @IBOutlet weak var tableRowLabel: WKInterfaceLabel!
+    
+}
--- HikersWatch WatchKit Extension/InterfaceController.swift
+++ HikersWatch WatchKit Extension/InterfaceController.swift
@@ -16,23 +16,31 @@ class InterfaceController: WKInterfaceController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     var userlocationInfo = [String]()
     
+    @IBOutlet weak var table: WKInterfaceTable!
+    
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         let locationArray = locations as NSArray
         let location = locationArray.lastObject as! CLLocation
         
         userlocationInfo.removeAll(keepCapacity: true)
         //Get latitude
-        userlocationInfo.append("\(location.coordinate.latitude)")
+        userlocationInfo.append("Lat: \(location.coordinate.latitude)")
         //Get longitude
-        userlocationInfo.append("\(location.coordinate.longitude)")
+        userlocationInfo.append("Lon: \(location.coordinate.longitude)")
         //Get altitude
-        userlocationInfo.append("\(location.altitude)")
+        userlocationInfo.append("Alt: \(location.altitude)")
         //Get speed
-        userlocationInfo.append("\(location.speed)")
+        userlocationInfo.append("Speed: \(location.speed)")
         //Get course
-        userlocationInfo.append("\(location.course)")
+        userlocationInfo.append("Course: \(location.course)")
         
         println(userlocationInfo)
+        
+        table.setNumberOfRows(userlocationInfo.count, withRowType: "tableRowController")
+        for (index, value) in enumerate(userlocationInfo) {
+            let row = table.rowControllerAtIndex(index) as! tableRowController
+            row.tableRowLabel.setText(value)
+        }
     }
 
     override func awakeWithContext(context: AnyObject?) {
